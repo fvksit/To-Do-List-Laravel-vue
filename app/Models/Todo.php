61c58f1133928763b800a3794f352d45f9843ea0
fvksit <dafikerja1@gmail.com>
2025-03-12 11:56:46 +0700

    make table Todo

---
+++ app/Models/Todo.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'todos';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'details',
+        'status',
+        'category_id',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function dates()
+    {
+        return $this->hasMany(Date::class);
+    }
+}
--- database/migrations/2025_03_12_034343_create_categories_table.php
+++ database/migrations/2025_03_12_034343_create_categories_table.php
@@ -14,6 +14,7 @@ return new class () extends Migration {
             $table->id();
             $table->string('name'); // add name column example: 'PHP', 'JavaScript', 'Python'
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
