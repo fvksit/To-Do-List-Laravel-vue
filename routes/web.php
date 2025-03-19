3a310879134a3a6ec08e26f9795cdb5d44a8ed62
fvksit <dafikerja1@gmail.com>
2025-03-19 13:35:36 +0700

    fix development conflict

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,8 @@ Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
 
-<<<<<<< HEAD
 Route::get('/{any}', function () {
     return view('welcome');
 })->where('any', '.*');
-=======
+
 require __DIR__.'/auth.php';
->>>>>>> backend
